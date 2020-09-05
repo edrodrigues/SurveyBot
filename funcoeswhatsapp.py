c7906585d73c68f64baf175ab2b91b05169e341f
Edmilson Rodrigues <ed.ufpe@gmail.com>
2020-09-05 10:38:32 -0300

    Update funcoeswhatsapp.py

--- funcoeswhatsapp.py
+++ funcoeswhatsapp.py
@@ -8,7 +8,7 @@ def enviaMsg(msg,num):
     # Your Account Sid and Auth Token from twilio.com/console
     # DANGER! This is insecure. See http://twil.io/secure
     account_sid = 'ACf359d87d80dec8e234eb846a8401d673'
-    auth_token = '754c82f5670c5935e1805b1d0af8499c'
+    auth_token = '[DELETED]'
 
 
     client = Client(account_sid, auth_token)
